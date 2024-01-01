c6341ec2191a8cf77b8312200df52c5a6d8109c9
masha82 <olive120691@gmail.com>
2024-01-01 19:46:04 +0700

    UPDATE

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        if (Auth::user()->level==1){
+            return view('home');
+        } elseif(Auth::user()->level==2){
+            return redirect()->route('graduate.create');
+        } elseif(Auth::user()->level==3){
+            return redirect()->route('gotakeg.create');
+        }
+        
     }
 }
